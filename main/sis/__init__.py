d126e4b3b2e69b1c3062cf870f342c39387475ca
Joseph Corpuz <josephjr.corpuz@g.msuiit.edu.ph>
2025-08-05 15:57:04 +0800

    working now

--- main/sis/__init__.py
+++ main/sis/__init__.py
@@ -10,11 +10,14 @@ application = Flask(__name__)
 
 
 # Configure application
+
 application.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 application.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_DATABASE_HOST")
 application.config["MYSQL_DATABASE_USER"] = os.getenv("MYSQL_DATABASE_USER")
 application.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("MYSQL_DATABASE_PASSWORD")
 application.config["MYSQL_DATABASE_DB"] = os.getenv("MYSQL_DATABASE_DB")
+print("Connecting as:", application.config["MYSQL_DATABASE_USER"])
+print("Password:", application.config["MYSQL_DATABASE_PASSWORD"])
 
 # Initialize MySQL
 my_sql = MySQL()
